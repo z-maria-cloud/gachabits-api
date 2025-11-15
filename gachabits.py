246bc44c8b67f64c385ec3dcb301855130880c22
z-maria-cloud <197367564+z-maria-cloud@users.noreply.github.com>
2025-11-15 17:42:51 +0100

    added main api route

--- gachabits.py
+++ gachabits.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, url_for
-from random import randint
+import random
 import json
 
 app = Flask(__name__)
+
+def r(items):
+    return random.choice(items)
+
+def g(what):
+	return r(gacha[what])
+
+limit = 20
 
 gacha = ""
 with open('gachabits.json', 'r') as file:
@@ -11,3 +19,20 @@ with open('gachabits.json', 'r') as file:
 @app.route("/")
 def home_route():
 	return gacha
+
+@app.route("/gacha/<what>/<int:num>")
+def main_func(what, num):
+	if what not in gacha.keys():
+		return f"Gachabits does not contain a key named {what}"
+	
+	if num == 0:
+		num = 1
+	elif num > limit:
+		num = limit
+	
+	final = []
+	
+	for i in range(num):
+		current = g(what)
+		final.append(current)
+	return final
